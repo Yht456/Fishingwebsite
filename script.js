12425c552fa30b61d6a26ae7fa3a77c847570d39
Yht456 <104627820+Yht456@users.noreply.github.com>
2022-06-20 09:26:05 +0100

    uj

--- script.js
+++ script.js
@@ -6,16 +6,13 @@ var   heroPosterImg = document.querySelector('.heroPosterImg'),
       startSecs = 10,
       endSecs = 15,
       secPaused = 0,
-      s = 0,
-      r = 0,
       playBtn = document.querySelectorAll('.playBtn')[0];
 tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var player,player2;
+var player;
 function onYouTubeIframeAPIReady() {
-  if(window.scrollY / heroPosterImg.getBoundingClientRect().height < 0.6){
     player = new YT.Player("player",{
       videoId: "1wkPMUZ9vX4",
       playerVars: {
@@ -37,32 +34,10 @@ function onYouTubeIframeAPIReady() {
       }
     });
   } 
-  /*PLACE FOR THE BOTTOM CODE*/
-        /*SECOND VIDEO*/
-        player2 = new YT.Player("player2",{
-          videoId: "nqye02H_H6I",
-          playerVars: {
-            'playsinline': 1,
-             controls:'0',
-             autoplay:1,
-             autohide:1,
-             showinfo:0,
-             modestbranding:1,
-             loop:1,
-             rel:0,
-             enablejsapi:1,
-             start:startSecs,
-             end:endSecs,
-          },
-          events: {
-            onReady:readyFuncTech,
-            onStateChange:stateFunTech
-          }
-        })
-      }
 
 /*1ST VIDEO FUNCTIONS*/
 function whenReadyFunc(event){
+  console.log('ready')
   setTimeout(function(){
   event.target.mute();
   event.target.playVideo();
@@ -88,165 +63,3 @@ function onStateChnageFunc(event){
       player.seekTo(startSecs);
   }  
 }
-
-/*2ND VIDEO FUNCTIONS*/
-function readyFuncTech(event){
-  window.onscroll = function(){
-    if(Math.abs(techPoster.getBoundingClientRect().top) <= techPoster.getBoundingClientRect().height / 3){
-      setTimeout(function(){
-    event.target.mute();
-    event.target.playVideo();
-  },2000) 
-    }else{
-      console.log('not')
-    }
-  }
-}
-function stateFunTech(event){
-  if(event.data == YT.PlayerState.PLAYING){
-    techPoster.querySelector('.poster').classList.add('fade');
-    window.onscroll = function(){
-      if(Math.abs(techPoster.getBoundingClientRect().top) >= techPoster.getBoundingClientRect().height / 3){
-        event.target.pauseVideo();
-      }else{
-        event.target.playVideo();
-      }
-    }
-  }
-  if(event.data == YT.PlayerState.ENDED){
-      event.target.seekTo(startSecs);
-  }  
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*window.onscroll = function(){
-  if((Math.abs(techPoster.getBoundingClientRect().height) - Math.abs(techPoster.getBoundingClientRect().top)) / Math.abs(techPoster.getBoundingClientRect().height) > 0.5){
-              
-    }
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*if(window.scrollY / heroPosterImg.getBoundingClientRect().height > 0.6){
-    window.onscroll = function(){
-      if(s == 0){
-        if(window.scrollY / heroPosterImg.getBoundingClientRect().height < 0.6){
-          s = 1;
-          player = new YT.Player("player",{
-            videoId: "1wkPMUZ9vX4",
-            playerVars: {
-              'playsinline': 1,
-              controls:'0',
-              autoplay:1,
-              autohide:1,
-              showinfo:0,
-              modestbranding:1,
-              loop:1,
-              rel:0,
-              enablejsapi:1,
-              start:startSecs,
-              end:endSecs,
-              origin:'http://localhost:80'
-            },
-            events: {
-              onReady:whenReadyFunc,
-              onStateChange:onStateChnageFunc
-            }
-        });
-        }
-      }
-    }
-  }*/
